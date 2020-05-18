1d25b70e6b7044ac2b59d48f81857ac42a7e1bf4
Roger Niederhauser <roger.niederhauser@siemens.com>
2020-05-18 21:53:12 +0200

    feat: finish elapsedtime format

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,22 @@
 #include <string>
+#include <sstream>
 
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
+string Filler(int n) {
+    return n < 10 ? "0" : "";
+}
+
+// DONE: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+    int hours = seconds / 3600;
+    int minutes = (seconds % 3600) / 60;
+    int s = seconds % 60;
+    std::stringstream ss;
+    ss << Filler(hours) << hours << ":" << Filler(minutes) << minutes << ":" << Filler(s) << s;
+    return ss.str();
+}
